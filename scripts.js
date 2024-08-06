0fa4ce2ce53835e50f971b35245fc875e11f8a50
massitd <82796634+massitd@users.noreply.github.com>
2024-08-06 18:28:30 -0400

    Create a properly laid out grid of divs.

--- scripts.js
+++ scripts.js
@@ -1,36 +1,22 @@
 const gridContainer = document.querySelector("div");
 
-
-// add flexbox properties to gridContainer
 gridContainer.style.display = "flex";
 gridContainer.style.justifyContent = "column";
 
-//add flexbox properties to rowContainer
-rowContainer.style.display = "flex";
-rowContainer.style.justifyContent = "";
 
-
-// use for loop to create a row of 16 50px width 
-// 50px length divs
 function gridRow() {
-    for (let index = 0; index < 16; index++){
-        let square = document.createElement("div");
-        square.style.width = "50px";
-        square.style.height = "50px";
-        square.style.backgroundColor = "black";
-        square.style.margin = "3px";
-        rowContainer.appendChild(square);
+    for (let index = 0; index < 16; index ++) {
+        let rowContainer = document.createElement("div");
+        gridContainer.appendChild(rowContainer);   
+        for (let indexA = 0; indexA < 16; indexA++) {
+            let square = document.createElement("div");
+            square.style.width = "50px";
+            square.style.height = "50px";
+            square.style.backgroundColor = "black";
+            square.style.margin = "1px";
+            rowContainer.appendChild(square);
+        };
     };
 };
 
 gridRow();
-
-// use for loop to create a column of 16 of these 
-function gridColumn() {
-    for (let index = 0; index < 16; index ++) {
-        let rowContainer = document.querySelector("#rowContainer");
-        gridContainer.appendChild(rowContainer);
-    }
-}
-
-gridColumn();
