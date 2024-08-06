4669b594a6fd964f6ca028df2b78d6b28ba13966
massitd <82796634+massitd@users.noreply.github.com>
2024-08-06 18:13:59 -0400

    Save this point. Stuck on this step of the project.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,24 @@
 const gridContainer = document.querySelector("div");
-const rowContainer = document.createElement("div");
-const square = document.createElement("div");
 
-gridContainer.appendChild(rowContainer);
 
 // add flexbox properties to gridContainer
 gridContainer.style.display = "flex";
+gridContainer.style.justifyContent = "column";
 
 //add flexbox properties to rowContainer
 rowContainer.style.display = "flex";
-rowContainer.style.justifyContent = "spaceAround";
+rowContainer.style.justifyContent = "";
 
-// add styles to square
-square.style.width = "50px";
-square.style.height = "50px";
-square.style.backgroundColor = "black";
 
 // use for loop to create a row of 16 50px width 
 // 50px length divs
 function gridRow() {
     for (let index = 0; index < 16; index++){
+        let square = document.createElement("div");
+        square.style.width = "50px";
+        square.style.height = "50px";
+        square.style.backgroundColor = "black";
+        square.style.margin = "3px";
         rowContainer.appendChild(square);
     };
 };
@@ -29,6 +28,9 @@ gridRow();
 // use for loop to create a column of 16 of these 
 function gridColumn() {
     for (let index = 0; index < 16; index ++) {
-        rowContainer.appendChild(rowContainer);
+        let rowContainer = document.querySelector("#rowContainer");
+        gridContainer.appendChild(rowContainer);
     }
 }
+
+gridColumn();
